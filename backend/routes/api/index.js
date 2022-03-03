25ec7e7d4b995d9184c56c1aa8b0e3c1c61386c2
Nikolas <nikolas.garza5@gmail.com>
2022-03-03 13:39:07 -0800

    can login with user credentials

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
+const asyncHandler = require("express-async-handler");
+const { setTokenCookie, restoreUser } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
 
-router.post("/test", function (req, res) {
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+router.post("/test", (req, res) => {
     res.json({ requestBody: req.body });
 });
 
