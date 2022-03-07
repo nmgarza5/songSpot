0870a52587c1a775768260d4ad61341ae0422189
Nikolas <nikolas.garza5@gmail.com>
2022-03-07 13:20:02 -0800

    basic layout for activity page setup

--- frontend/src/components/ActivityPage/index.js
+++ frontend/src/components/ActivityPage/index.js
@@ -6,7 +6,22 @@ const ActivityPage = () => {
             <div className="activity-container">
                 <div className="main-container">
                     Main container
-                    <div className="inner-main">Recently Played</div>
+                    <div className="inner-main">
+                        <h2>Recently Played</h2>
+                        <div className="song-container">
+                            <div className="song">Song</div>
+                            <div className="song">Song</div>
+                            <div className="song">Song</div>
+                            <div className="song">Song</div>
+                            <div className="song">Song</div>
+                            <div className="song">Song</div>
+                            <div className="song">Song</div>
+                            <div className="song">Song</div>
+                            <div className="song">Song</div>
+                            <div className="song">Song</div>
+                        </div>
+                        <button className="see-more-btn">See More...</button>
+                    </div>
                     <div className="inner-main">Charts: Top 50 Songs</div>
                     <div className="inner-main">Your Top Songs</div>
                 </div>
