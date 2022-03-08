946f3787a3548d8ac9f7a81cf479b59fea8fe665
Nikolas <nikolas.garza5@gmail.com>
2022-03-07 20:20:29 -0800

    routes for songs started. get working.

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,14 +1,57 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
-const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { Song } = require("../../db/models");
+const { restoreUser } = require("../../utils/auth");
+const { Song, User } = require("../../db/models");
 
 const router = express.Router();
 
 router.get(
     "/",
-    requireAuth,
     asyncHandler(async (req, res) => {
-        const songs = await db.Song.findAll();
+        const songs = await Song.findAll();
+        res.json(songs);
     })
 );
+router.post(
+    "/",
+    restoreUser,
+    asyncHandler(async (req, res) => {
+        const userId = req.user.id;
+        const genreId = req.body.genre;
+        const title = req.body.title;
+        const imageUrl = req.body.imageUrl;
+        const audioUrl = req.body.audioUrl;
+
+        await Song.create({
+            userId,
+            genreId,
+            title,
+            imageUrl,
+            audioUrl,
+        });
+        res.redirect("/");
+    })
+);
+
+router.put(
+    "/:id",
+    restoreUser,
+    asyncHandler(async (req, res) => {
+        const userId = req.user.id;
+        const genreId = req.body.genre;
+        const title = req.body.title;
+        const imageUrl = req.body.imageUrl;
+        const audioUrl = req.body.audioUrl;
+        const songId = req.params.id;
+
+        const song = await Song.findByPk(songId);
+        song.genreId = genreId;
+        song.title = title;
+        song.imageUrl = imageUrl;
+        song.audioUrl = audioUrl;
+        await review.save();
+        res.json({ song });
+    })
+);
+
+module.exports = router;
