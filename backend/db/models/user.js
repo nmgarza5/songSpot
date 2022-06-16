139e784f533ba9d3bf3ab8f93c9790640419947f
Nikolas <nikolas.garza5@gmail.com>
2022-06-16 15:29:25 -0700

    update

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -60,6 +60,7 @@ module.exports = (sequelize, DataTypes) => {
         User.hasMany(models.Song, { foreignKey: "userId" });
         User.hasMany(models.Playlist, { foreignKey: "userId" });
         User.hasMany(models.SongLike, { foreignKey: 'userId' });
+
         User.hasMany(models.PlaylistLike, { foreignKey: 'userId' });
     };
 
