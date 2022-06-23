054ca7d9d7812a9d3ed8990cedec70c5b768f8e6
Nikolas <nikolas.garza5@gmail.com>
2022-06-23 12:56:03 -0700

    likes sidebar complete

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -2,8 +2,29 @@ import "./HomePage.css";
 import SongList from "../SongList";
 import { NavLink } from "react-router-dom";
 import Playlists from "../Playlists";
+import { useSelector } from "react-redux";
+import LikeCard from "../LikeCard";
 
-const HomePage = () => {
+const HomePage = ({songs, playlists}) => {
+
+    const sessionUser = useSelector((state) => state.session.user);
+
+    let userLikes = [];
+
+    songs.forEach((song) => {
+        let songLike = song.SongLikes.find((like) => like.userId === sessionUser.id)
+        if (songLike) userLikes.push(song)
+    })
+
+    playlists.forEach((playlist) => {
+        let playlistLike = playlist.PlaylistLikes.find((like) => like.userId === sessionUser.id)
+        if (playlistLike) userLikes.push(playlist)
+    })
+
+    let recentLikes = userLikes.sort((a,b) => (new Date(b.createdAt) - new Date(a.createdAt))).slice(0, 5);
+
+
+
     return (
         <>
             <div className="activity-container">
@@ -17,9 +38,7 @@ const HomePage = () => {
                     </div>
                     <div className="inner-main">
                         <h2>Recently Added Playlists</h2>
-
                         <Playlists className="song" />
-
                         <NavLink className="see-more" to="/playlists">
                             <button>See More</button>
                         </NavLink>
@@ -27,15 +46,16 @@ const HomePage = () => {
                 </div>
                 <div className="side-container">
                     <div className="inner-side">
-                        <h2> Coming Soon! </h2>
-                        <h2> Likes </h2>
-                        <i className="fa-regular fa-heart icon"></i>
+                        <h2> Recent Likes </h2>
+                        {recentLikes.map((like) => (
+                            <LikeCard like={like} />
+                        ))}
                     </div>
-                    <div className="inner-side">
+                    {/* <div className="inner-side">
                         <h2> Coming Soon! </h2>
                         <h2> Listening History </h2>
                         <i className="fa-brands fa-soundcloud icon"></i>
-                    </div>
+                    </div> */}
                 </div>
             </div>
             ;
